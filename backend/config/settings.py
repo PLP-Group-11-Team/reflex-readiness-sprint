ef44685e213298c3a845d4a950fd4badda278df6
manmungadr-creator <manmunga.dr@gmail.com>
2026-09-01 23:31:03 +0300

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -157,8 +157,14 @@ REST_FRAMEWORK = {
     ],
 }
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3001',
-    'http://127.0.0.1:3001',
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "https://reflex-readiness-sprint-t45m.vercel.app",
+]
+
+CSRF_TRUSTED_ORIGINS = [
     "https://reflex-readiness-sprint-t45m.vercel.app",
 ]
 
